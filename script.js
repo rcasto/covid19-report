ccc740f25d65f4b4176464a5e7566e09e08b4931
Richie Casto <rcasto92@gmail.com>
2020-03-03 19:40:48 -0800

    cache report in memory, add update report worker

--- script.js
+++ script.js
@@ -62,77 +62,79 @@ function getColumns(data, columns) {
             .reduce((dataCols, col) => dataCols.concat(dataRow[col]), []));
 }
 
-fetch(covid19DailyReportsDirectoryUrl)
-    .then(response => response.json())
-    .then(dailyReports =>
-        dailyReports.filter(dailyReport =>
-            dailyReport.type === 'file' &&
-            csvFileRegex.test(dailyReport.name)))
-    .then(dailyReports =>
-        dailyReports.map(dailyReport => dailyReport.name))
-    .then(dailyReports => dailyReports.sort((report1, report2) => {
-        const report1Date = getReportDate(report1);
-        const report2Date = getReportDate(report2);
-        return report2Date - report1Date;
-    }))
-    .then(dailyReports => {
-        if (dailyReports.length > 0) {
-            return dailyReports[0];
-        }
-        throw new Error('No daily reports found...hmmm');
-    })
-    .then(latestReport => `${covid19DailyReportsDirectoryUrl}/${latestReport}`)
-    .then(fetchAndParseReport)
-    .then(async parsedLatestReport => {
-        try {
-            const timeSeriesReport = await fetchAndParseReport(covid19ConfirmedReportsTimeSeriesUrl);
-            const latestReportData = getColumns(parsedLatestReport, [
-                'Province/State',
-                'Country/Region',
-                'Confirmed',
-                'Deaths',
-                'Recovered'
-            ]);
-            const timeSeriesData = getColumns(timeSeriesReport, [
-                'Province/State',
-                'Country/Region',
-                'Lat',
-                'Long'
-            ]);
+module.exports = function () {
+    return fetch(covid19DailyReportsDirectoryUrl)
+        .then(response => response.json())
+        .then(dailyReports =>
+            dailyReports.filter(dailyReport =>
+                dailyReport.type === 'file' &&
+                csvFileRegex.test(dailyReport.name)))
+        .then(dailyReports =>
+            dailyReports.map(dailyReport => dailyReport.name))
+        .then(dailyReports => dailyReports.sort((report1, report2) => {
+            const report1Date = getReportDate(report1);
+            const report2Date = getReportDate(report2);
+            return report2Date - report1Date;
+        }))
+        .then(dailyReports => {
+            if (dailyReports.length > 0) {
+                return dailyReports[0];
+            }
+            throw new Error('No daily reports found...hmmm');
+        })
+        .then(latestReport => `${covid19DailyReportsDirectoryUrl}/${latestReport}`)
+        .then(fetchAndParseReport)
+        .then(async parsedLatestReport => {
+            try {
+                const timeSeriesReport = await fetchAndParseReport(covid19ConfirmedReportsTimeSeriesUrl);
+                const latestReportData = getColumns(parsedLatestReport, [
+                    'Province/State',
+                    'Country/Region',
+                    'Confirmed',
+                    'Deaths',
+                    'Recovered'
+                ]);
+                const timeSeriesData = getColumns(timeSeriesReport, [
+                    'Province/State',
+                    'Country/Region',
+                    'Lat',
+                    'Long'
+                ]);
 
-            // Build lookup map for location of province/country from time series data
-            // Will add this data to latest report entries
-            const provinceCountryToLocationMap = new Map();
-            timeSeriesData.forEach(timeSeriesEntry => {
-                const [province, country, lat, long] = timeSeriesEntry;
-                provinceCountryToLocationMap.set(`${province}-${country}`, [lat, long]);
-            });
+                // Build lookup map for location of province/country from time series data
+                // Will add this data to latest report entries
+                const provinceCountryToLocationMap = new Map();
+                timeSeriesData.forEach(timeSeriesEntry => {
+                    const [province, country, lat, long] = timeSeriesEntry;
+                    provinceCountryToLocationMap.set(`${province}-${country}`, [lat, long]);
+                });
 
-            // Add location info (lat, long) to all latest report entries
-            latestReportData.forEach(latestReportEntry => {
-                const [province, country] = latestReportEntry;
-                const location = provinceCountryToLocationMap.get(`${province}-${country}`);
-                latestReportEntry.push.apply(latestReportEntry, location);
-            });
+                // Add location info (lat, long) to all latest report entries
+                latestReportData.forEach(latestReportEntry => {
+                    const [province, country] = latestReportEntry;
+                    const location = provinceCountryToLocationMap.get(`${province}-${country}`);
+                    latestReportEntry.push.apply(latestReportEntry, location);
+                });
 
-            // Add new header row - Updated with Lat and Long
-            latestReportData.unshift([
-                'Province/State',
-                'Country/Region',
-                'Confirmed',
-                'Deaths',
-                'Recovered',
-                'Lat',
-                'Long'
-            ]);
+                // Add new header row - Updated with Lat and Long
+                latestReportData.unshift([
+                    'Province/State',
+                    'Country/Region',
+                    'Confirmed',
+                    'Deaths',
+                    'Recovered',
+                    'Lat',
+                    'Long'
+                ]);
 
-            // Stringify CSV data and write/update latest on disk
-            const latestReportCsvString = await stringifyPromise(latestReportData);
-            await fsWriteAsync(covid19ReportOutputPath, latestReportCsvString, {
-                encoding: 'utf8',
-            });
-        } catch (err) {
-            console.error(err);
-        }
-    })
-    .catch(console.error);
+                // Stringify CSV data and write/update latest on disk
+                const latestReportCsvString = await stringifyPromise(latestReportData);
+                await fsWriteAsync(covid19ReportOutputPath, latestReportCsvString, {
+                    encoding: 'utf8',
+                });
+            } catch (err) {
+                console.error(err);
+            }
+        })
+        .catch(console.error);
+}
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const util = require('util');
 const parse = require('csv-parse');
+const cron = require('node-cron');
+const updateReport = require('./script');
 
 const parsePromise = util.promisify(parse);
 const fileReadPromise = util.promisify(fs.readFile);
@@ -9,6 +11,8 @@ const fileReadPromise = util.promisify(fs.readFile);
 const covid19ReportOutputPath = 'covid19-report.csv';
 const port = process.env.PORT || 3000;
 const app = express();
+
+let latestReport = null;
 
 app.use(express.static('public'));
 
@@ -18,11 +22,10 @@ app.get('/', (req, res) => {
 
 app.get('/api/latest-report', async (req, res) => {
     try {
-        const fileContents = await fileReadPromise(covid19ReportOutputPath, {
-            encoding: 'utf8',
-        });
-        const fileContentsParsed = await parsePromise(fileContents);
-        res.json(fileContentsParsed);
+        if (!latestReport) {
+            await readReport();
+        }
+        res.json(latestReport);
     } catch (err) {
         res.status(500);
         res.send(JSON.stringify(err));
@@ -31,3 +34,23 @@ app.get('/api/latest-report', async (req, res) => {
 
 app.listen(port,
     () => console.log(`Server started on port ${port}`));
+
+// Worker script section
+async function readReport() {
+    const fileContents = await fileReadPromise(covid19ReportOutputPath, {
+        encoding: 'utf8',
+    });
+    latestReport = await parsePromise(fileContents);
+}
+
+function initUpdateWorker() {
+    // Runs once an hour
+    cron.schedule('0 * * * *', () => {
+        updateReport()
+            .then(async () => {
+                await readReport();
+            });
+    });
+}
+
+initUpdateWorker();
